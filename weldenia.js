ac2988daee78990d0d96ae1748e03fa2c26a76fd
Pilar <pilyrodriguez0@gmail.com>
2026-08-10 23:15:44 -0600

    Update weldenia.js

--- weldenia.js
+++ weldenia.js
@@ -1,11 +1,12 @@
-paper.insrall(window);
-window.addEventListener('load', function () {
-  paper.setup('flowerCanvas');
-  const circle = new Path.Circle(
-    view.center,
-    50
-    );
-  cicle.fillColor = 'yellow';
-});
+paper.install(window);
 
-                        
+window.addEventListener('load', function () {
+    paper.setup('flowerCanvas');
+
+    const circle = new Path.Circle(
+        view.center,
+        50
+    );
+
+    circle.fillColor = 'yellow';
+});

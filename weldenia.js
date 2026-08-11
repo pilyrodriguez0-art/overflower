bc3cf78dd673a898da3d12e9641029f09ce194fe
Pilar <pilyrodriguez0@gmail.com>
2026-08-10 22:49:33 -0600

    Test Paper.js with yellow circle
    Test Paper.js with yellow circle

---
+++ weldenia.js
@@ -0,0 +1,11 @@
+paper.insrall(window);
+window.addEventListener('load', function () {
+  paper.setup('flowerCanvas');
+  const circle = new Path.Circle(
+    view.center,
+    50
+    );
+  cicle.fillColor = 'yellow';
+});
+
+                        
